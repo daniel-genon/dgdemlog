440a21400b45703fbb5d3d996b28d22f1a648af1
Daniel Génon <78256578+daniel-genon@users.noreply.github.com>
2025-07-11 18:13:14 +0200

    Delete creerprojet directory

--- creerprojet/tbdcreerprojet.cpp
+++
@@ -1,7 +0,0 @@
-#include "creerprojet.h"
-
-int main(int argc, char* argv[])
-{
-  auto app = Gtk::Application::create("org.dgdemlog.boitedialog.projet");
-  return app->make_window_and_run<CreerProjet>(argc, argv);
-}
